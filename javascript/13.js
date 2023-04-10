8a903e37b295f027a6cf62cd394936eb446d6818
Min <sou05094@gmail.com>
2023-04-10 13:13:20 +0900

    04/10수업

---
+++ javascript/13.js
@@ -0,0 +1,8 @@
+function show() {
+    //alert("안녕하세요");
+    document.getElementById("h2id").innerHTML = "Hi";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("h2id").innerHTML = "Hi";
+});
